d3de3a7fb1809e671d0d96d55474a784d4d273ee
Artem Meleshko <service.leshko@gmail.com>
2024-10-12 22:13:11 +0200

    fixed build scripts and targets

--- build_core.sh
+++ build_core.sh
@@ -2,44 +2,67 @@
 
 set -e
 
-## Build TwitterKit.framework - x86_64
-xcodebuild \
+# Variables
+FRAMEWORK_NAME="TwitterCore"
+OUTPUT_DIR="./output"
+IOS_ARCHIVE_PATH="./build/ios"
+SIMULATOR_ARCHIVE_PATH="./build/simulator"
+CATALYST_ARCHIVE_PATH="./build/catalyst"
+XCFRAMEWORK_PATH="./${FRAMEWORK_NAME}.xcframework"
+
+# Clean up any previous builds
+rm -rf "$OUTPUT_DIR" "$XCFRAMEWORK_PATH"
+mkdir -p "$OUTPUT_DIR"
+
+# Build for iOS device (armv7, arm64) - iOS 12.0+
+xcodebuild archive \
     -project TwitterCore/TwitterCore.xcodeproj \
-    -scheme TwitterCore -configuration Debug \
-    -sdk "iphonesimulator" \
-    HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphonesimulator/Headers $(pwd)/TwitterCore/iphonesimulator/PrivateHeaders"  \
-    CONFIGURATION_BUILD_DIR=./iphonesimulator \
-	ENABLE_BITCODE=NO \
-    clean build
+    -scheme $FRAMEWORK_NAME \
+    -sdk iphoneos \
+    -archivePath "${IOS_ARCHIVE_PATH}" \
+    -configuration Debug \
+    -destination 'generic/platform=iOS' \
+    -derivedDataPath ./build \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    SUPPORTS_MACCATALYST=NO
 
-## From: https://developer.apple.com/library/archive/qa/qa1940/_index.html
-# If code signing fails with the error "resource fork, Finder information, or similar detritus not allowed."
-#   e.g. /usr/bin/codesign --force --sign - --timestamp=none $(pwd)/TwitterCore/iphonesimulator/TwitterCore.framework
-# Try:
-#   xattr -lr /Users/SWARM5/Work/twitter-kit-ios-master/TwitterKit/iphonesimulator/TwitterKit.framework
-#   xattr -cr /Users/SWARM5/Work/twitter-kit-ios-master/TwitterKit/iphonesimulator/TwitterKit.framework
-
-## Build TwitterKit.framework - armv7, arm64
-xcodebuild \
+# Build for iOS Simulator (x86_64, arm64) - iOS 12.0+
+xcodebuild archive \
     -project TwitterCore/TwitterCore.xcodeproj \
-    -scheme TwitterCore -configuration Debug \
-    -sdk "iphoneos" \
-    HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphoneos/Headers $(pwd)/TwitterCore/iphoneos/PrivateHeaders"  \
-    CONFIGURATION_BUILD_DIR=./iphoneos \
-	ENABLE_BITCODE=NO \
-    clean build
+    -scheme $FRAMEWORK_NAME \
+    -sdk iphonesimulator \
+    -archivePath "${SIMULATOR_ARCHIVE_PATH}" \
+    -configuration Debug \
+    -destination 'generic/platform=iOS Simulator' \
+    -derivedDataPath ./build \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    SUPPORTS_MACCATALYST=NO
 
-## Merge into one TwitterCore.framework with x86_64, armv7, arm64
-rm -rf iOS
-mkdir -p iOS
-cp -r TwitterCore/iphoneos/TwitterCore.framework/ iOS/TwitterCore.framework
-lipo -create -output iOS/TwitterCore.framework/TwitterCore TwitterCore/iphoneos/TwitterCore.framework/TwitterCore TwitterCore/iphonesimulator/TwitterCore.framework/TwitterCore
-lipo -archs iOS/TwitterCore.framework/TwitterCore
+# Build for Mac Catalyst (x86_64, arm64) - iOS 13.0+
+xcodebuild archive \
+    -project TwitterCore/TwitterCore.xcodeproj \
+    -scheme $FRAMEWORK_NAME \
+    -sdk macosx \
+    -archivePath "${CATALYST_ARCHIVE_PATH}" \
+    -configuration Debug \
+    -destination 'generic/platform=macOS,variant=Mac Catalyst' \
+    -derivedDataPath ./build \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    SUPPORTS_MACCATALYST=YES
 
-## Zip them into TwitterKit.zip
-ZIP_FILE=TwitterCore.zip
+# Create XCFramework
+xcodebuild -create-xcframework \
+    -framework "${IOS_ARCHIVE_PATH}.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
+    -framework "${SIMULATOR_ARCHIVE_PATH}.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
+    -framework "${CATALYST_ARCHIVE_PATH}.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
+    -output "$XCFRAMEWORK_PATH"
+
+# Zip the XCFramework
+ZIP_FILE="${FRAMEWORK_NAME}.xcframework.zip"
 if test -f "$ZIP_FILE"; then
     rm "$ZIP_FILE"
 fi
-zip -r "$ZIP_FILE" iOS/*
-rm -rf iOS
+zip -r "$ZIP_FILE" "$XCFRAMEWORK_PATH"
